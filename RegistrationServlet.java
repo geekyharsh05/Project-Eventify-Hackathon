3e3f472451e30782c92fbda8ef7ac244ab5e6170
Dipendra Bhatta <103090861+dipenbhat557@users.noreply.github.com>
2023-04-29 19:05:18 +0530

    Add files via upload

---
+++ RegistrationServlet.java
@@ -0,0 +1,17 @@
+package com.hackathon;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegistrationServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException{
+		
+		
+		
+	}
+}
